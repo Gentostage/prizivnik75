53faf5a5fc57fdbb55dec1852b80c9917938c9f9
Stepan Minkin <34418402+1474net@users.noreply.github.com>
2019-03-10 15:32:00 +0900

    Плавный якорь и счетчик

--- static/js/init.js
+++ static/js/init.js
@@ -6,3 +6,15 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+  //плавная анимация 
+  $("a[href*=#]").on("click", function(e){
+      var anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $(anchor.attr('href')).offset().top-50
+      }, 777);
+      e.preventDefault();
+      return false;
+  });
+});
