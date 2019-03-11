d96b9043560bd6ceb5c11636a879ddb5c7f18f51
1474net <gentostage@gmail.com>
2019-03-11 15:16:15 +0900

    Шапка удаление лишнего

--- static/js/init.js
+++ static/js/init.js
@@ -12,9 +12,33 @@ $(document).ready(function(){
   $("a[href*=#]").on("click", function(e){
       var anchor = $(this);
       $('html, body').stop().animate({
-          scrollTop: $(anchor.attr('href')).offset().top-50
+          scrollTop: $(anchor.attr('href')).offset().top-150
       }, 777);
       e.preventDefault();
       return false;
   });
+
+  
 });
+
+$(document).ready(function(){
+
+if($(window).width() <= 768){
+  $('#lb_timer').removeClass('left-align');
+};
+if($(window).width() >= 768){
+  $('#lb_timer').addClass('left-align');
+};
+
+  
+});
+
+$(window).resize(function () {
+  // Удалить класс при изменение разрешения 
+  if($(window).width() <= 768){
+    $('#lb_timer').removeClass('left-align');
+  };
+  if($(window).width() >= 768){
+    $('#lb_timer').addClass('left-align');
+  };
+})
